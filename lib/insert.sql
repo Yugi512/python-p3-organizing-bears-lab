6401ed307ee01e4d1dd5a67d92774c8e0df12c17
Kelvin Gil <gilkelvin2@gmail.com>
2024-06-20 19:00:06 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age , sex , color , temperament , alive) VALUES ('Mr. Chocolate',1,'M','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Rowdy',12,'F','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Tabitha',11,'F','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Sergeant Brown',13,'M','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Melissa',14,'F','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Grinch',15,'M','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES ('Wendy',16,'F','Brown','normal',1);
+INSERT INTO bears (name , age , sex , color , temperament , alive ) VALUES (NULL,18,'M','Brown','normal',1);
